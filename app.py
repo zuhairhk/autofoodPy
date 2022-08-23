a7bdb156401b8c810e1adddcdf018622aa8d7d8b
zuhairhk <zuhairhk@gmail.com>
2022-08-23 15:21:48 -0400

    locate update

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, request
+import locate
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/', methods=['POST'])
+def getvalue():
+    data = request.form['query']
+
+    rlist = locate.main(data)
+
+    return render_template('passed.html', query = data, list = rlist)
+    print(data)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- locate.py
+++ locate.py
@@ -4,49 +4,53 @@ import pandas as pd
 import time
 import os
 
-API_KEY = os.environ['API_KEY']
-map_client = googlemaps.Client(API_KEY)
-
 def miles_to_metres(miles):
     try:
         return miles * 1609.344
     except:
         return 0
 
-#loc = (36.133123, -115.137338)
-g = geocoder.ip('me')
-loc = (g.latlng[0], g.latlng[1])
+def main(query):
 
-search_string = input('Enter desired cuisine: ') # 'fries'
-distance = miles_to_metres(5)
-restaruant_list = []
+    API_KEY = os.environ['API_KEY']
+    map_client = googlemaps.Client(API_KEY)
 
-response = map_client.places_nearby(
-    location=loc,
-    keyword=search_string,
-    # name='fries shop',
-    radius=distance
-)
+    #loc = (36.133123, -115.137338)
+    g = geocoder.ip('me')
+    loc = (g.latlng[0], g.latlng[1])
 
-print(response)
+    search_string = query # input('Enter desired cuisine: ') # 'fries'
+    distance = miles_to_metres(5)
+    restaruant_list = []
 
-restaruant_list.extend(response.get('results'))
-next_page_token = response.get('next_page_token')
-
-while next_page_token:
-    time.sleep(2)
     response = map_client.places_nearby(
         location=loc,
         keyword=search_string,
         # name='fries shop',
-        radius=distance,
-        page_token=next_page_token
+        radius=distance
     )
+
+    print(response)
+
     restaruant_list.extend(response.get('results'))
     next_page_token = response.get('next_page_token')
 
-df = pd.DataFrame(restaruant_list)
-df['url'] = 'https://www.google.com/maps/place/?q=place_id:' + df['place_id']
+    while next_page_token:
+        time.sleep(2)
+        response = map_client.places_nearby(
+            location=loc,
+            keyword=search_string,
+            # name='fries shop',
+            radius=distance,
+            page_token=next_page_token
+        )
+        restaruant_list.extend(response.get('results'))
+        next_page_token = response.get('next_page_token')
 
-df.to_excel('response_list.xlsx', index=False)
-print(df)
+    df = pd.DataFrame(restaruant_list)
+    df['url'] = 'https://www.google.com/maps/place/?q=place_id:' + df['place_id']
+
+    df.to_excel('response_list.xlsx', index=False)
+    print(df)
+
+    return restaruant_list
