3d700d8dcb74367eb0105267f0fbcb9a451ac4e3
zuhairhk <zuhairhk@gmail.com>
2022-08-26 13:26:37 -0400

    Frontend Update

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request
 import locate
+import random
+import os
 
 app = Flask(__name__)
 
@@ -8,13 +10,44 @@ def index():
     return render_template('index.html')
 
 @app.route('/', methods=['POST'])
-def getvalue():
+def display():
     data = request.form['query']
 
-    rlist = locate.main(data)
+    rlists = locate.main(data)[0]
+    links = locate.main(data)[1]
+    photo_refs = locate.main(data)[3]
 
-    return render_template('passed.html', query = data, list = rlist)
-    print(data)
+    rannum = random.randint(0, len(rlists) - 1)
+
+    rlist = rlists[rannum]
+    link = links[rannum]
+    loc_name = locate.main(data)[2][rannum]
+
+    open_now = locate.main(data)[4][rannum]
+
+    if open_now['open_now'] == True:
+        open_status = 'Open'
+    else:
+        open_status = 'Closed'
+    
+    try:
+        photo_ref = photo_refs[rannum][0]['photo_reference']
+    except:
+        photo_ref = 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/1024px-No_image_available.svg.png'
+        print('No image available')
+
+    return render_template(
+        'passed.html', 
+        query = data, 
+        list = rlist, 
+        link = link,
+        photo_ref = photo_ref,
+        loc_name = loc_name,
+        API_KEY = os.environ['API_KEY'],
+        open_status = open_status,
+        rating = locate.main(data)[5][rannum],
+        vicinity = locate.main(data)[6][rannum],
+    )
 
 if __name__ == '__main__':
     app.run(debug=True)
--- locate.py
+++ locate.py
@@ -3,12 +3,37 @@ import geocoder
 import pandas as pd
 import time
 import os
+import requests
+from bs4 import BeautifulSoup
 
 def miles_to_metres(miles):
     try:
         return miles * 1609.344
     except:
         return 0
+
+def get_image(query):
+    url = "https://www.google.com/search?q=" + str(query) + "&source=lnms&tbm=isch"
+    headers = {'User-Agent': 'Mozilla/5.0'}
+
+    html = requests.get(url, headers=headers).text
+    print(html)
+
+    soup = BeautifulSoup(html, 'html.parser')
+    # image = soup.find("img",{"class":"t0fcAb"})
+    images = soup.find_all('img')
+
+    resUrl = []
+
+    for image in images:
+        src = image.get('src')
+        resUrl.append(requests.compat.urljoin(url, src))
+
+    '''if not image:
+        return
+    return image['src']'''
+
+    return resUrl
 
 def main(query):
 
@@ -36,7 +61,7 @@ def main(query):
     next_page_token = response.get('next_page_token')
 
     while next_page_token:
-        time.sleep(2)
+        time.sleep(1)
         response = map_client.places_nearby(
             location=loc,
             keyword=search_string,
@@ -50,7 +75,15 @@ def main(query):
     df = pd.DataFrame(restaruant_list)
     df['url'] = 'https://www.google.com/maps/place/?q=place_id:' + df['place_id']
 
-    df.to_excel('response_list.xlsx', index=False)
+    # df.to_excel('response_list.xlsx', index=False)
     print(df)
 
-    return restaruant_list
+    return (
+        restaruant_list,
+        df['url'],
+        df['name'],
+        df['photos'],
+        df['opening_hours'],
+        df['rating'],
+        df['vicinity']
+    )
